bf2ee8a127a1495ce314302a3d0405b5f8522baa
Bruno Gomes <brunojabs@gmail.com>
2019-08-17 14:49:00 -0300

    Add how it works section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import "@magnetis/astro"
 import React from "react"
 import styled from "styled-components"
 
+const MOBILE_SIZE = "480px"
+const TABLET_SIZE = "768px"
+const DESKTOP_SIZE = "1200px"
+
 const Container = styled.div`
   display: flex;
 `
@@ -13,7 +17,7 @@ const Nav = styled.nav`
   justify-content: flex-end;
   height: 60px;
   align-items: center;
-  background-color: transparent;
+  background-color: var(--color-space-100);
   position: fixed;
   width: 100%;
   padding: 20px;
@@ -47,13 +51,17 @@ const Sections = styled.div`
   flex-direction: column;
 `
 
-const Hero = styled.div`
+const Section = styled.div`
   display: flex;
-  padding-top: 60px;
   align-items: center;
   justify-content: center;
   flex-direction: column;
   text-align: center;
+`
+
+const Hero = styled(Section)`
+  padding-top: 60px;
+  justify-content: space-around;
 `
 
 const Highlight = styled.span`
@@ -63,22 +71,55 @@ const Highlight = styled.span`
 `
 
 const CallToAction = styled.button`
+  margin: 50px 0;
   &:hover {
     background-color: var(--color-sun-500);
     border-color: var(--color-sun-300);
   }
 `
 
+const HowItWorks = styled(Section)`
+  padding: 50px;
+  justify-content: space-between;
+`
+
+const Steps = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin: 50px 0;
+  max-width: 1200px;
+`
+
+const Step = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding: 30px;
+  background: var(--gradient-hoag);
+  margin: 0 30px;
+  align-items: center;
+  overflow: auto;
+  border-radius: 5%;
+  & > * {
+    margin: 5px 0;
+  }
+`
+
+const Arrow = styled.i`
+  align-self: center;
+`
+
 export default () => (
   <Container>
     <Nav>
-      <LogoLink className="a-btn a-btn--medium">Validata</LogoLink>
-      <NavLink href="#how-it-works" className="a-btn a-btn--medium">
+      <LogoLink className="a-btn a-btn--large">Validata</LogoLink>
+      <NavLink href="#how-it-works" className="a-btn a-btn--large">
         How it Works
       </NavLink>
-      <NavLink className="a-btn a-btn--medium">Pricing</NavLink>
-      <NavLink className="a-btn a-btn--medium">Who are we?</NavLink>
-      <Register className="a-btn a-btn--small">Register</Register>
+      <NavLink className="a-btn a-btn--large">Pricing</NavLink>
+      <NavLink className="a-btn a-btn--large">Who are we?</NavLink>
+      <Register className="a-btn a-btn--medium">Register</Register>
     </Nav>
     <Sections>
       <Hero>
@@ -95,6 +136,43 @@ export default () => (
         </p>
         <CallToAction className="a-btn a-btn--large">Start now</CallToAction>
       </Hero>
+      <HowItWorks>
+        <h2 className="a-title--large">How it works</h2>
+        <Steps>
+          <Step>
+            <i className="a-icon a-icon__additional a-icon--size-large" />
+            <h3 className="a-text--large">Create a project</h3>
+            <p className="a-text--medium">
+              Create a Project (web, mobile, blog) and register our webhook with
+              your analytics provider
+            </p>
+          </Step>
+          <Arrow className="a-icon a-icon__drop-right a-icon--size-large" />
+
+          <Step>
+            <i className="a-icon a-icon__sliders a-icon--size-large" />
+            <h3 className="a-text--large">Specify events</h3>
+            <p className="a-text--medium">
+              Create the specifications for each event by setting what data
+              would you like us to validate
+            </p>
+          </Step>
+          <Arrow className="a-icon a-icon__drop-right a-icon--size-large" />
+          <Step>
+            <i className="a-icon a-icon__mail a-icon--size-large" />
+            <h3 className="a-text--large">Be notified</h3>
+            <p className="a-text--medium">
+              We start validate events as they come and notify you if there is
+              anything wrong
+            </p>
+          </Step>
+        </Steps>
+      </HowItWorks>
+
+      <HowItWorks>
+        <h2 className="a-title--large">Pricing</h2>
+        <Steps></Steps>
+      </HowItWorks>
     </Sections>
   </Container>
 )
